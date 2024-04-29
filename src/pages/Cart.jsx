32a44d171087d75a42261f9dde6df1ad9cb681ad
Nand Makadia <nand.makadia@streebo.com>
2024-04-29 10:31:23 +0530

    cart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <h3>Cart</h3>
+      <div className="cartWrapper">
+        {
+
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Cart;
